328e1fc8160bd37a1cefef4839c4f931a81da19c
Gabriel Hartmann de Azeredo <gabriel.hazeredo@gmail.com>
2023-02-02 15:09:36 -0300

    Updates

---
+++ precache-manifest.5344948f40aff957f2da1cd9e1f6bc1d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3eae0f1136f89667f925be36983dbe1",
+    "url": "/visionai-landing-page/index.html"
+  },
+  {
+    "revision": "e4706a784957c5bc1b24",
+    "url": "/visionai-landing-page/static/css/main.85acbb5f.chunk.css"
+  },
+  {
+    "revision": "17cc3cee5b44316dcae1",
+    "url": "/visionai-landing-page/static/js/2.89088abe.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/visionai-landing-page/static/js/2.89088abe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4706a784957c5bc1b24",
+    "url": "/visionai-landing-page/static/js/main.8b6964f8.chunk.js"
+  },
+  {
+    "revision": "cd9f735087307b1c544e",
+    "url": "/visionai-landing-page/static/js/runtime-main.6a85e91b.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/visionai-landing-page/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/visionai-landing-page/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/visionai-landing-page/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/visionai-landing-page/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/visionai-landing-page/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/visionai-landing-page/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/visionai-landing-page/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/visionai-landing-page/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/visionai-landing-page/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/visionai-landing-page/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/visionai-landing-page/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/visionai-landing-page/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/visionai-landing-page/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
